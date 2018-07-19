63bcd8617078908ff315a6549789a29feb4b8de8
ecejmsr <ecejmsr>
2018-07-19 17:46:42 -0400

    first commit search display

---
+++ app/src/main/java/com/batch/mcs/finalproject/BaseFragment.java
@@ -0,0 +1,12 @@
+package com.batch.mcs.finalproject;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BaseFragment extends Fragment {
+
+    public BaseFragment() {}
+
+}
---
+++ app/src/main/java/com/batch/mcs/finalproject/SearchFragment.java
@@ -0,0 +1,18 @@
+package com.batch.mcs.finalproject;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SearchFragment extends BaseFragment {
+
+    public SearchFragment() {}
+
+}

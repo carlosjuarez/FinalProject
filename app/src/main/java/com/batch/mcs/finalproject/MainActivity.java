5cd423b6dc13f505eaa8c6d5cef8a51d61b086f6
Carlos.Juarez <cjuarez@estrategiatec.com.mx>
2018-07-19 15:44:31 -0400

    added activity class for this branch

---
+++ app/src/main/java/com/batch/mcs/finalproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.batch.mcs.finalproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

2798a850e8e3729864f02fda81d7cfe754908a29
LAPTOP-JJVFQ0SH\ADMIN <aloss003@fiu.edu>
2018-07-21 01:10:49 -0400

    Fixed fragment class to exclude comments and unnecessary code. Fixed some hardcoded values in xmls.

---
+++ app/src/main/java/com/batch/mcs/finalproject/ChatUserInteractionFragment.java
@@ -0,0 +1,23 @@
+package com.batch.mcs.finalproject;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ChatUserInteractionFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public ChatUserInteractionFragment() {
+
+    }
+
+}

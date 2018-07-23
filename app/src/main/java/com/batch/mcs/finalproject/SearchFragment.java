dbb0fe1ed1cdab586adfd091fbc19335db85ad91
ecejmsr <ecejmsr>
2018-07-23 13:26:21 -0400

    deleted comments

--- app/src/main/java/com/batch/mcs/finalproject/SearchFragment.java
+++ app/src/main/java/com/batch/mcs/finalproject/SearchFragment.java
@@ -2,14 +2,6 @@ package com.batch.mcs.finalproject;
 
 import android.support.v4.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SearchFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SearchFragment extends BaseFragment {
 
     public SearchFragment() { }

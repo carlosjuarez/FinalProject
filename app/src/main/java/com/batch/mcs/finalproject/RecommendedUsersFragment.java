2737add08f76de53220f58f92aa94382b8e0b656
onlyalf <alvarolossada@hotmail.com>
2018-07-23 19:45:35 -0400

    Created xml for recommended users fragment.

---
+++ app/src/main/java/com/batch/mcs/finalproject/RecommendedUsersFragment.java
@@ -0,0 +1,12 @@
+package com.batch.mcs.finalproject;
+
+import android.support.v4.app.Fragment;
+
+public class RecommendedUsersFragment extends Fragment {
+
+
+    public RecommendedUsersFragment() {
+
+    }
+
+}

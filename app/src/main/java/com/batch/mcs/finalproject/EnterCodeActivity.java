a53c826e378dad112259a73c7c55feda87f8b4f6
Your Name <you@example.com>
2018-07-24 02:40:54 -0400

    Created Enter Code Display

---
+++ app/src/main/java/com/batch/mcs/finalproject/EnterCodeActivity.java
@@ -0,0 +1,13 @@
+package com.batch.mcs.finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnterCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_code);
+    }
+}

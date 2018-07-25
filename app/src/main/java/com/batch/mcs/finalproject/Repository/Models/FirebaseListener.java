22fb511a461b18d0bebccb310116ba301ef60f36
onlyalf <alvarolossada@hotmail.com>
2018-07-24 21:22:10 -0400

    Created implementation for livedata in users creating a parent class FirebaseListener which extends livedata and implements custom listener to use to update data on query.

---
+++ app/src/main/java/com/batch/mcs/finalproject/Repository/Models/FirebaseListener.java
@@ -0,0 +1,38 @@
+package com.batch.mcs.finalproject.Repository.Models;
+
+import android.arch.lifecycle.LiveData;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+public class FirebaseListener extends LiveData{
+
+    class RealtimeListener implements ValueEventListener {
+
+        Query query;
+
+        public RealtimeListener(){
+
+        }
+
+        public void setQuery(Query query) {
+            this.query = query;
+        }
+
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            setValue(dataSnapshot);
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+            Log.e("FIREBASE ", "Can't listen to query " + query, databaseError.toException());
+        }
+    }
+
+
+
+}

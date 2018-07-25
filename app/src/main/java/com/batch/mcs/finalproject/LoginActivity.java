549c1501c2f189d946b99a84eb875b9f4c30cffa
Carlos.Juarez <cjuarez@estrategiatec.com.mx>
2018-07-24 21:02:00 -0400

    add method for firebase authentication

--- app/src/main/java/com/batch/mcs/finalproject/LoginActivity.java
+++ app/src/main/java/com/batch/mcs/finalproject/LoginActivity.java
@@ -3,11 +3,23 @@ package com.batch.mcs.finalproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.batch.mcs.finalproject.authentication.FirebaseAuthentication;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        try{
+            mAuth = FirebaseAuth.getInstance();
+            mAuth.sendPasswordResetEmail("juvcarl@gmail.com");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/com/batch/mcs/finalproject/authentication/FirebaseAuthentication.java
@@ -0,0 +1,13 @@
+package com.batch.mcs.finalproject.authentication;
+
+import com.google.firebase.auth.ActionCodeSettings;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class FirebaseAuthentication {
+
+    public boolean sendRecoveryPassword(String email){
+
+        return true;
+    }
+
+}

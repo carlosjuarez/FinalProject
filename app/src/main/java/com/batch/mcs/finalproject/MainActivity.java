9d4183f23da7f76906a7fb3cc3d9654010e1a5fd
Carlos.Juarez <cjuarez@estrategiatec.com.mx>
2018-07-30 19:20:35 -0400

    merge functionality of tablayout into mainactivity

--- app/src/main/java/com/batch/mcs/finalproject/MainActivity.java
+++ app/src/main/java/com/batch/mcs/finalproject/MainActivity.java
@@ -4,6 +4,7 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.batch.mcs.finalproject.databinding.ActivityMainBinding;
 import com.batch.mcs.finalproject.viewmodel.AppViewModel;
@@ -18,11 +19,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         ActivityMainBinding activityBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setSupportActionBar((Toolbar) activityBinding.toolbarlayout.findViewById(R.id.toolbar));
 
         String userId = Objects.requireNonNull(getIntent().getExtras()).getString(getString(R.string.parameter_userid));
 
         AppViewModel appViewModel = ViewModelProviders.of(this).get(AppViewModel.class);
         appViewModel.init(userId);
+
         TabViewModel tabViewModel = new TabViewModel(this);
         activityBinding.setTabViewModel(tabViewModel);
 

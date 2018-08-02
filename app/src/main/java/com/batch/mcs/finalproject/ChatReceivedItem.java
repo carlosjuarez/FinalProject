6432e35063b0f98c503ee774265589090c17c09f
onlyalf <alvarolossada@hotmail.com>
2018-08-02 14:06:16 -0400

    commented out message and chat in init method for appviewmodel

--- app/src/main/java/com/batch/mcs/finalproject/ChatReceivedItem.java
+++ app/src/main/java/com/batch/mcs/finalproject/ChatReceivedItem.java
@@ -1,10 +1,14 @@
 package com.batch.mcs.finalproject;
 
+import com.batch.mcs.finalproject.models.Message;
+
 public class ChatReceivedItem implements ChatItem {
     private String text;
+    Message message;
 
-    public ChatReceivedItem(String text){
-        this.text = text;
+    public ChatReceivedItem(Message message){
+        this.message = message;
+        this.text = message.getContent();
     }
 
     public String getText(){
@@ -14,6 +18,7 @@ public class ChatReceivedItem implements ChatItem {
     public void setText(String text){
         this.text = text;
     }
+
     @Override
     public int geChatItemType() {
         return ChatItem.RECEIVED;
--- app/src/main/java/com/batch/mcs/finalproject/ChatSentItem.java
+++ app/src/main/java/com/batch/mcs/finalproject/ChatSentItem.java
@@ -1,11 +1,14 @@
 package com.batch.mcs.finalproject;
 
+import com.batch.mcs.finalproject.models.Message;
+
 public class ChatSentItem implements ChatItem {
     private String text;
+    Message message;
 
-    public ChatSentItem(String text){
-        this.text = text;
-
+    public ChatSentItem(Message message){
+        this.message = message;
+        this.text = message.getContent();
     }
 
     public String getText(){
